6dfe94d7e25ec461aa5bf8c0ca83fbba040808f4
Allan <contato@allancruz.com.br>
2024-07-25 12:57:39 -0300

    feat:  enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001', // ou '*' para permitir todas as origens
+  });
+
   // Pipes
   app.useGlobalPipes(
     new ValidationPipe({
