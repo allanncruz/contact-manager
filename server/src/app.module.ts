2da0854dc4f41c54fab80333948e6ea3e2c7faeb
Allan <contato@allancruz.com.br>
2024-07-25 15:44:24 -0300

    feat: import group module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, GroupModule],
   controllers: [AppController],
   providers: [
     AppService,
